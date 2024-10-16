b76d7337c3df070d3334d01bde3bdf12bfc0b465
Igor Aguiar <desenvigor@gmail.com>
2024-10-16 17:48:23 -0300

    Questões 02-11 manipulação DOM

--- Desafio-150_Questões/Manipulação-DOM/script.js
+++ Desafio-150_Questões/Manipulação-DOM/script.js
@@ -1,5 +1,3 @@
-
-
 function calculaMedia(){
     let nota1 = document.getElementById("nota-1").value;
     let nota2 = document.getElementById("nota-2").value;
@@ -9,4 +7,157 @@ function calculaMedia(){
     let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4))/4;
     document.getElementById('media').innerText = media;    
     document.getElementById('resultado').innerText = media >= 7 ? "Aprovado" : "Reprovado" 
+}
+
+function isNumeroPrimo(){
+    let number = document.getElementById('number').value;
+    let result = true;
+    if (number <= 1){
+        result = false;
+    }
+
+    else if (number == 2){
+        result = true;
+    }
+
+    else if( number % 2 == 0){
+        result = false
+    }
+
+
+    for (let i = 3; i <= Math.sqrt(number); i += 2){
+        if (number % i == 0){
+            result = false;
+            break;
+        } 
+    }
+
+    document.getElementById('resultado').innerText = result ? `Numero ${number} é Primo` : `Numero ${number} Não é Primo`
+}
+
+function verificaNumeroAdivinhado(){
+    let num = parseInt(sessionStorage.getItem('sortNum'));
+    let valorInserido = parseInt(document.getElementById('numero').value);
+    document.getElementById('resultado').innerText = num == valorInserido ? "Parabéns você acertou" : valorInserido < num ? "O número inserido é menor" : "O numero inserido é maior"
+}
+
+function geraNumeroAleatorio(){
+    sessionStorage.setItem('sortNum', Math.floor(Math.random() * 100))
+    document.getElementById('numberStatus').innerText = "Numero Gerado"
+}
+
+function somarNumeros(){
+    let topNum = parseInt(document.getElementById('number').value);
+    let result = 0;
+    for (let i = 0; i <= topNum ; i++){
+        result += i;
+    }
+
+    document.getElementById('resultado').innerText = result;
+}
+
+function gerarTabuada(){
+    let val = parseInt(document.getElementById('number').value)
+
+    for (let i = 1 ; i <= 10 ; i++){
+       let novalinha = document.createElement('tr');
+       let base = document.createElement('td');
+       base.innerText = i;
+
+       let primeiraCx = document.createElement('td');
+       primeiraCx.innerText = i-val;
+       primeiraCx.setAttribute('id', `subtracao_${i}`)
+
+       let segundaCx = document.createElement('td');
+       segundaCx.innerText = i+val;
+       segundaCx.setAttribute('id', `adicao_${i}`)
+
+       let terceiraCx = document.createElement('td');
+       terceiraCx.innerText = i*val;
+       terceiraCx.setAttribute('id', `multiplicacao_${i}`)
+       
+       let quartaCx = document.createElement('td');
+       quartaCx.innerText = (i/val).toFixed(2);
+       quartaCx.setAttribute('id', `divisao_${i}`)
+
+       novalinha.appendChild(base);
+       novalinha.appendChild(primeiraCx);
+       novalinha.appendChild(segundaCx);
+       novalinha.appendChild(terceiraCx);
+       novalinha.appendChild(quartaCx);
+
+       
+       document.getElementById('tabuada').appendChild(novalinha);
+    }
+}
+
+function verificarNomeMaisLongo(){
+    let nomes = document.getElementById('nomes').value
+    let arrNomes = nomes.split(',')
+    let maiorNome = '';
+    arrNomes.map(str => str.length > maiorNome.length ? maiorNome = str : null);
+
+    let novoParagrafo = document.createElement('h3');
+    novoParagrafo.innerText = `O Maior nome é ${maiorNome}`
+    document.body.appendChild(novoParagrafo);
+}
+
+function setCalculoFatorial(){
+    let number = parseInt(document.getElementById('number').value);
+    let resultado = document.createElement('h2');
+    
+    resultado.innerText = "Resultado é: " + fatorial(number);
+
+    document.body.appendChild(resultado)
+}
+
+function fatorial(num){
+    if (num == 0){
+        return 1;
+    }else {
+        return num * this.fatorial(num - 1)
+    }
+}
+
+function sortearDados(){
+    let num1 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    let num2 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+    document.getElementById("dado_1").innerText = num1;
+    document.getElementById("dado_2").innerText = num2;
+    document.getElementById("soma").innerText = num2 + num1;
+
+}
+
+function calcularLetras(){
+    let textoCompleto = document.getElementById('text').value;
+    textoCompleto = textoCompleto.replace(/[^\p{L}\p{N}\s]/gu, '');
+    textoCompleto = textoCompleto.replace(/\s+/g, '');
+    let consoantes = textoCompleto.split('').filter(letra => /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g.test(letra));
+
+    document.getElementById('consoantes').innerText = consoantes.length;
+    document.getElementById('vogais').innerText = textoCompleto.length - consoantes.length;
+}
+
+function converToBinary(){
+    let resultado = document.createElement('h3')
+    console.log(parseInt(document.getElementById('number').value).toString(2))
+    resultado.innerText = parseInt(document.getElementById('number').value).toString(2);
+
+    document.body.appendChild(resultado)
+}
+
+function sequenciaDeFibonacci(){
+    let a = 0, b = 1, temp;
+    let lastNum = parseInt(document.getElementById('number').value);
+    let values = [a, b]
+
+    for (let i = 2 ; i < lastNum ; i++){
+        temp = b + a;
+        a = b
+        b = temp
+        values.push(b)
+    }
+
+    document.getElementById('fib').innerText = values.toString()
 }
