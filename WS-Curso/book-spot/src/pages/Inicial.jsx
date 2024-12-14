dcfa577b2641cfeb23409b9a3613ac38c3dfd894
Igor Aguiar <desenvigor@gmail.com>
2024-12-14 10:34:25 -0300

    Criação de página inicial navbar

---
+++ WS-Curso/book-spot/src/pages/Inicial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicial = () => {
+  return (
+    <div>
+        <h1>Bem vindo ao Book Spot</h1>
+        <p>Descubra resenhas incríveis de livros!</p>
+    </div>
+  )
+}
+
+export default Inicial
